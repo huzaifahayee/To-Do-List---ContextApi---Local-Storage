b6520d5ea8bf2b836e464e8c012cc3835c3f5f92
Muhammad Huzaifa <huzaifahayee751@gmail.com>
2026-07-05 20:58:01 +0500

    Added functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import {todoProvider} from "./contexts"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos,setTodos] = useState([]);
+
+  const addTodo = (todo) =>{
+    setTodos((prev)=>[{id: Date.now(), ...todo},...prev])
+  }
+
+  const updateTodo = (id,todo) =>{
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo :prevTodo ) ))
+  }
+
+const deleteTodo = (id) => {
+  setTodos((prev) => prev.filter((todo) => todo.id !== id))
+
+}
+const toggleCompelete = (id) => {
+  setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prev, completed: !prevTodo.completed} :prevTodo))
+}
+
 
   return (
-    <>
+    <todoProvider value={{todos,addTodo, updateTodo, deleteTodo, toggleCompelete}}>
         <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
@@ -20,7 +35,7 @@ function App() {
                     </div>
                 </div>
             </div>
-    </>
+    </todoProvider>
   )
 }
 
